9c87cf56d2ae0d326fbafb1abceabd386800478b
Yahor Paromau <egor.latvia@gmail.com>
2018-10-15 23:48:19 +0200

    Text added

--- core/src/com/greenelephant/babylon/Babylon.java
+++ core/src/com/greenelephant/babylon/Babylon.java
@@ -2,18 +2,23 @@ package com.greenelephant.babylon;
 
 import com.badlogic.gdx.ApplicationAdapter;
 import com.badlogic.gdx.Gdx;
+import com.badlogic.gdx.graphics.Color;
 import com.badlogic.gdx.graphics.GL20;
 import com.badlogic.gdx.graphics.Texture;
+import com.badlogic.gdx.graphics.g2d.BitmapFont;
 import com.badlogic.gdx.graphics.g2d.SpriteBatch;
 
 public class Babylon extends ApplicationAdapter {
 	SpriteBatch batch;
 	Texture img;
+	BitmapFont font;
 	
 	@Override
 	public void create () {
 		batch = new SpriteBatch();
 		img = new Texture("green-elephant.png");
+		font = new BitmapFont();
+		font.setColor(Color.GREEN);
 	}
 
 	@Override
@@ -21,7 +26,8 @@ public class Babylon extends ApplicationAdapter {
 		Gdx.gl.glClearColor(0, 0, 0, 1);
 		Gdx.gl.glClear(GL20.GL_COLOR_BUFFER_BIT);
 		batch.begin();
-		batch.draw(img, 0, 0);
+		batch.draw(img, 200, 200);
+		font.draw(batch, "Green Elephant", 200, 150);
 		batch.end();
 	}
 	
@@ -29,5 +35,6 @@ public class Babylon extends ApplicationAdapter {
 	public void dispose () {
 		batch.dispose();
 		img.dispose();
+		font.dispose();
 	}
 }
