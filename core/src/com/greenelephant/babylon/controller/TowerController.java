60ff037c4d1d130d41c44f77aea023f0ef3e36bd
nikolaiser <nickolayser@gmail.com>
2018-10-20 14:15:37 +0200

    some comments deleted

--- core/src/com/greenelephant/babylon/controller/TowerController.java
+++ core/src/com/greenelephant/babylon/controller/TowerController.java
@@ -5,9 +5,6 @@ import com.badlogic.gdx.math.Polygon;
 
 public class TowerController {
 
-    private Polygon towerBounds;
-    private float width;
-    private float height;
     private long shootingFrequency;
     private long lastShoot;
 
@@ -21,14 +18,6 @@ public class TowerController {
         this.shootingFrequency = shootingFrequency;
     }
 
-    /*public void handle(OrthographicCamera camera) {
-        if (Gdx.input.isButtonPressed(Input.Buttons.LEFT)) {
-            Vector3 touchPos = new Vector3(Gdx.input.getX(), Gdx.input.getY(), 0);
-            camera.unproject(touchPos);
-            towerBounds.setPosition(touchPos.x - width / 2,
-                    touchPos.y - height /2 );
-        }
-    }*/
 
 
     public boolean shoot(long multiplier) {
